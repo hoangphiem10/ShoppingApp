897ee3d125804265612a3062ead69bb0e2792675
Hoang Phiem <tuhoangphiem10@gmail.com>
2022-05-01 22:33:40 +0700

    Initial Commit

---
+++ Shopping App/Shopping_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_AppApp.swift
+//  Shopping App
+//
+//  Created by thphiem on 01/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Shopping_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
